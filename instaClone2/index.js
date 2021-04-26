ad98662d82b9d32c81b3c3601e8844568f8526f1
IgorBaio <igorbaiosoares@gmail.com>
2021-04-26 13:42:37 -0300

    Dependencias

--- instaClone2/index.js
+++ instaClone2/index.js
@@ -1,8 +1,10 @@
 import { registerRootComponent } from 'expo';
+import Navig from './src/Navigator'
 
-import App from './App';
+import axios from 'axios'
 
+axios.defaults.baseURL= 'https://instaclone-11300-default-rtdb.firebaseio.com/'
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Navig);
